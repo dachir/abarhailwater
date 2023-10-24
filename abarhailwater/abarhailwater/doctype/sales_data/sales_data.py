29f6f8ba91e8a2219a0f501bfdf48fed95f98e81
dachir <dodziamouzou@gmail.com>
2023-10-24 19:17:03 +0000

    241020232023

--- abarhailwater/abarhailwater/doctype/sales_data/sales_data.py
+++ abarhailwater/abarhailwater/doctype/sales_data/sales_data.py
@@ -3,7 +3,10 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe.exceptions import LinkValidationError
 import traceback
+import csv
+import os
 
 class SalesData(Document):
 	
@@ -13,25 +16,54 @@ class SalesData(Document):
         doctype = first_item.get("doctype")
         fieldnames = frappe.get_meta(doctype).get_valid_columns()
         product_names = fieldnames[26:40]
-        try:
-            for d in self.details:
+
+        # Create a list to store the failed records
+        failed_records = []
+        failed_numbers = []
+        num = ''
+
+        for d in self.details:
+            try:
+                num = d.billno
                 invoice_details = []
                 for n in product_names:
-                    if int(d.get(n)) > 0 :
-                        details = frappe._dict({
-                            "item_code": frappe.get_meta(doctype).get_label(n), 
-                            #"description": n,
-                            "qty": int(d.get(n)),
-                            #"rate": item.prix,
-                            "doctype": "Sales Invoice Item",
-                        })
-                        invoice_details.append(details)
+                    if int(d.get(n)) > 0:
+                        max_qty = int(d.get(n))
+                        item_code = frappe.get_meta(doctype).get_label(n)
+                        batches = frappe.db.get_list("Batch", fields=["name", "batch_qty"], filters={"item":item_code, "batch_qty": [">",0]}, order_by="batch_qty desc")
+                        for b in batches:
+                            if b.batch_qty > max_qty:
+                                details = frappe._dict({
+                                    "item_code": item_code,
+                                    "qty": max_qty,
+                                    "doctype": "Sales Invoice Item",
+                                })
+                                invoice_details.append(details)
+                                max_qty = 0
+                                break
+                            else:
+                                details = frappe._dict({
+                                    "item_code": item_code,
+                                    "qty": b.batch_qty,
+                                    "doctype": "Sales Invoice Item",
+                                })
+                                invoice_details.append(details)
+                                max_qty = max_qty - b.batch_qty
+
+                        if max_qty > 0:
+                            details = frappe._dict({
+                                "item_code": item_code,
+                                "qty": max_qty,
+                                "doctype": "Sales Invoice Item",
+                            })
+                            invoice_details.append(details)
+
 
                 args = frappe._dict(
                     {
                         "customer": d.customercode,
                         "company": self.company,
-                        "set_posting_time" : 1,
+                        "set_posting_time": 1,
                         "posting_date": d.billdate,
                         "currency": self.currency,
                         "branch": self.branch,
@@ -40,13 +72,13 @@ class SalesData(Document):
                         "docstatus": 0,
                         "update_stock": 1,
                         "sales_data": self.name,
-                        "items":invoice_details,
+                        "items": invoice_details,
                     }
                 )
 
-                if details :
+                if invoice_details:
                     tax = frappe._dict({
-                        "charge_type": "On Net Total", 
+                        "charge_type": "On Net Total",
                         "account_head": "VAT 15% - AHW",
                         "description": "VAT 15% @ 15.0",
                         "rate": 15.0,
@@ -55,13 +87,24 @@ class SalesData(Document):
                     args.update({"taxes": [tax]})
                     sale = frappe.get_doc(args)
                     sale.insert()
-        except Exception as e:
-            # Get the full traceback
-            full_traceback = traceback.format_exc()
+                    
+            except Exception as e:
+                # Add the failed record to the list
+                failed_records.append(args)
+                frappe.msgprint(num)
 
-            # Display the full traceback
-            error_message = "Error: " + str(e) + "\n" + "Data: " + str(args) + "\n" + "Error occurred during Sales Invoice insertion. All records are rejected." + "\n" + full_traceback
-            frappe.throw(error_message)
+                # Send the error to log
+                frappe.log_error(e)
+
+        # Create a CSV file to store failed records
+        frappe.msgprint(str(failed_records))
+        if failed_records:
+            csv_file_path = os.path.join(frappe.get_site_path("public", "files"), "failed_sales_data.csv")
+
+            with open(csv_file_path, "w", newline="") as csv_file:
+                csv_writer = csv.DictWriter(csv_file, fieldnames=args.keys())
+                csv_writer.writeheader()
+                csv_writer.writerows(failed_records)
 
     def on_cancel(self):
         #self.ignore_linked_doctypes = "GL Entry"
