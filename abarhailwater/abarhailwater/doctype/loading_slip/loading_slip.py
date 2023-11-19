5aeb77ffe72709ed19dd53f44880faec568c79d0
dachir <dodziamouzou@gmail.com>
2023-11-19 10:24:45 +0000

    191120231124

--- abarhailwater/abarhailwater/doctype/loading_slip/loading_slip.py
+++ abarhailwater/abarhailwater/doctype/loading_slip/loading_slip.py
@@ -62,6 +62,7 @@ class LoadingSlip(Document):
                         "company": self.company,
                         "set_posting_time" : 1,
                         "posting_date": d.slipdate,
+                        "posting_time": "23:50",
                         "from_warehouse": self.source_warehouse,
                         "branch": self.branch,
                         "doctype": "Stock Entry",
