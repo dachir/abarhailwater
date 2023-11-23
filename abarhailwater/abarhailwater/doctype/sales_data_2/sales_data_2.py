b86cedd3dcc70336eef3e182fd935d21bbf16b31
dachir <dodziamouzou@gmail.com>
2023-11-23 18:17:15 +0000

    231120231917

--- abarhailwater/abarhailwater/doctype/sales_data_2/sales_data_2.py
+++ abarhailwater/abarhailwater/doctype/sales_data_2/sales_data_2.py
@@ -106,29 +106,30 @@ class SalesData2(Document):
 				if b.qty <= 0:
 					continue
 
-				if b.qty >= max_qty:
-					details = frappe._dict({
-						"item_code": d.productname,
-						"qty": max_qty,
-						"rate" : float(d.grossamount) / float(d.quantity),
-						"doctype": "Sales Invoice Item",
-					})
-					invoice_details.append(details)
-					b.qty -= max_qty
-					max_qty = 0
-					temp_batches[temp_batches.index(b)] = b
-					break
-				else:
-					details = frappe._dict({
-						"item_code": d.productname,
-						"qty": b.qty,
-						"rate" : float(d.grossamount) / float(d.quantity),
-						"doctype": "Sales Invoice Item",
-					})
-					invoice_details.append(details)
-					max_qty = max_qty - b.qty
-					b.qty = 0
-					temp_batches[temp_batches.index(b)] = b
+				while b.qty > 0 :
+					if b.qty >= max_qty:
+						details = frappe._dict({
+							"item_code": d.productname,
+							"qty": max_qty,
+							"rate" : float(d.grossamount) / float(d.quantity),
+							"doctype": "Sales Invoice Item",
+						})
+						invoice_details.append(details)
+						b.qty -= max_qty
+						max_qty = 0
+						temp_batches[temp_batches.index(b)] = b
+						break
+					else:
+						details = frappe._dict({
+							"item_code": d.productname,
+							"qty": b.qty,
+							"rate" : float(d.grossamount) / float(d.quantity),
+							"doctype": "Sales Invoice Item",
+						})
+						invoice_details.append(details)
+						max_qty = max_qty - b.qty
+						b.qty = 0
+						temp_batches[temp_batches.index(b)] = b
 
 			if max_qty > 0:
 				details = frappe._dict({
